610d83ca7fd39c574895f7be3cdc22664783935a
ehsan gharibi <stc30stc@gmail.com>
2025-01-14 18:43:06 +0330

    verj2

---
+++ tamrin5.js
@@ -0,0 +1,16 @@
+/*const email = "stc30stc@gmail.com";
+const username = email.split('@')[0];
+console.log(username);*/
+
+/*const email = "erfan_yusefi@gmail.com";
+const username = email.split('@')[0];
+console.log(username);*/
+
+/*const email = "milad.botostart@gmail.com";
+const username = email.split('@')[0];
+console.log(username);*/
+
+
+/*const numbers = ['+989129450190', '+989191695744'];
+const number = numbers[1].slice(-10);
+console.log(number);*/
