840228c638da8a399e956b6f0fc28f68281ba715
ehsan gharibi <stc30stc@gmail.com>
2025-01-15 23:11:05 +0330

    verz6

--- tamrin5.js
+++ tamrin5.js
@@ -14,3 +14,20 @@ console.log(username);*/
 /*const numbers = ['+989129450190', '+989191695744'];
 const number = numbers[1].slice(-10);
 console.log(number);*/
+
+const Dars1 = [ "d1 , d2 , d3 , d4" ]
+console.log (Dars1)
+console.log ( 8/2 )
+
+const x = 5 ;
+const y = 3 ;
+const z = 5 ;
+console.log (x == y);
+console.log (x == z);
+console.log (5 > 3);
+
+const f = "ehsan";
+const l = "gharibi";
+const full = f + l ;
+console.log (full)
+
