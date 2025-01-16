1b8c7c990c15f9548cb62cf515b716f870eed583
ehsan gharibi <stc30stc@gmail.com>
2025-01-16 13:17:22 +0330

    verz9

--- tamrin5.js
+++ tamrin5.js
@@ -1,53 +1,82 @@
 /*const email = "stc30stc@gmail.com";
 const username = email.split('@')[0];
 console.log(username);*/
-
 /*const email = "erfan_yusefi@gmail.com";
 const username = email.split('@')[0];
 console.log(username);*/
-
 /*const email = "milad.botostart@gmail.com";
 const username = email.split('@')[0];
 console.log(username);*/
-
-
 /*const numbers = ['+989129450190', '+989191695744'];
 const number = numbers[1].slice(-10);
 console.log(number);*/
-
 /*const Dars1 = [ "d1 , d2 , d3 , d4" ]
 console.log (Dars1)
 console.log ( 8/2 )
-
 const x = 5 ;
 const y = 3 ;
 const z = 5 ;
 console.log (x == y);
 console.log (x == z);
 console.log (5 > 3);
-
 const f = "ehsan";
 const l = "gharibi";
 const full = f + l ;
 console.log (full)*/
-
-const price1 = 10;
+/*const price1 = 10;
 const price2 = 15;
 const price3 = 20;
-
-const walet = 60 ;
-
+const walet = 65 ;
 const totalprice = price1 + price2 + price3 * 2 ;
 console.log ( "totalprice :" , totalprice )
-
 const averageprice = totalprice / 4 ;
 console.log ("totalprice :" , averageprice)
-
 const takhfif = totalprice * 0.95
 console.log ("takfife :" , takhfif)
-
 const canbuy = walet >= totalprice ;
 console.log ("mitonei bekhari :" , canbuy)
-
 const massage = canbuy ? "Afarin mitonei bekhari" : "seikteir nemitonei"; 
-console.log ("vazeiyat : ", massage)
+console.log ("vazeiyat : ", massage)*/ ;
+//number methods
+/*const number =50 ;
+console.log(number);
+console.log(typeof number ) ;
+const stirng = number.toString();
+console.log(stirng);
+console.log(typeof stirng);
+const number2 = 3.123456789 ;
+console.log(number2.toFixed(3))
+console.log(typeof stirng);
+const sring2 = "50";
+console.log (Number(sring2)) ;
+console.log(typeof stirng2);*/
+//console.log (text.length)
+//console.log (text[11] ,)
+//console.log (text.indexOf("i"))
+//console.log (text.charAt("4"))
+//console.log (text.split(" "));
+//console.log (text.replace("ehsan" , "ali"));
+/*const text = "ehsan gharibi is a inba developer"
+const text2 = "and instryactor";
+console.log (text+text2);*/
+/*const text = "ehsan gharibi is adame Good"
+const newtext =text.slice(6, 13)
+console.log (newtext)*/
+/*const text = "hi iam ehsan gharibi"
+const select1 = text.charAt();
+const CAPtext= select1.toUpperCase();
+const finaltext = text.replace(select1 , CAPtext)
+console.log(finaltext);*/
+
+/*const phon = "09191695744"
+const select4number = phon.slice(4, 8)
+const st = "@@@@"
+const starey = phon.replace( select4number,st);
+console.log (phon)
+console.log (select4number)
+console.log (starey)*/
+
+const fulname = "Ehsan Gharibi";
+const spaceTndes = fulname.indexOF (" ");
+
+console.log (fulname);
