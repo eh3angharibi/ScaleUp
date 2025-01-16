ad7a3045213ad5ee9b1ac31bb5803739babc564a
ehsan gharibi <stc30stc@gmail.com>
2025-01-16 11:12:42 +0330

    verz8

--- tamrin5.js
+++ tamrin5.js
@@ -15,7 +15,7 @@ console.log(username);*/
 const number = numbers[1].slice(-10);
 console.log(number);*/
 
-const Dars1 = [ "d1 , d2 , d3 , d4" ]
+/*const Dars1 = [ "d1 , d2 , d3 , d4" ]
 console.log (Dars1)
 console.log ( 8/2 )
 
@@ -29,5 +29,25 @@ console.log (5 > 3);
 const f = "ehsan";
 const l = "gharibi";
 const full = f + l ;
-console.log (full)
+console.log (full)*/
 
+const price1 = 10;
+const price2 = 15;
+const price3 = 20;
+
+const walet = 60 ;
+
+const totalprice = price1 + price2 + price3 * 2 ;
+console.log ( "totalprice :" , totalprice )
+
+const averageprice = totalprice / 4 ;
+console.log ("totalprice :" , averageprice)
+
+const takhfif = totalprice * 0.95
+console.log ("takfife :" , takhfif)
+
+const canbuy = walet >= totalprice ;
+console.log ("mitonei bekhari :" , canbuy)
+
+const massage = canbuy ? "Afarin mitonei bekhari" : "seikteir nemitonei"; 
+console.log ("vazeiyat : ", massage)
